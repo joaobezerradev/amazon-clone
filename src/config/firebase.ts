f7bcfdbf8f9eb06d103188e9bb8c14755f2ece07
Joao Vitor <vitor.lira98@gmail.com>
2021-01-03 18:50:53 -0300

    chore: remove firebase config

--- src/config/firebase.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyBbZZ6qJeO3GL7T8rD4J0GK2y6dBVlsYqU',
-  authDomain: 'clone-b2a7b.firebaseapp.com',
-  projectId: 'clone-b2a7b',
-  storageBucket: 'clone-b2a7b.appspot.com',
-  messagingSenderId: '112947402655',
-  appId: '1:112947402655:web:c8c122d1b824d1c5d00faa',
-};
